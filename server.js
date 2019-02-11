b4b6b921970ceedf8f6f92e92c8e3320040f6202
mike <fender18295@gmail.com>
2019-02-10 21:20:01 -0500

    added more cloud layers to landing page

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require("express");
 
 const app = express();
 
+// const landing = require("./public/html/landing.html");
+
 app.use(express.static("public"));
 
 app.set("view engine", "pug");
 app.set("views", "./layouts");
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
   res.render("main");
 });
 app.get("/bio", (req, res) => {
   res.render("./partials/bio");
 });
-
+app.get("/", (req, res) => {
+  res.sendFile(
+    "/home/mike/desktop/jsProjects/personal/server/public/html/landing.html"
+  );
+});
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
 });
